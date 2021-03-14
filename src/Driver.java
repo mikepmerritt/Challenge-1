9c5e226847dfc76b291327998ec93355a9777140
romwasserman <rom.wasserman@quinnipiac.edu>
2021-03-14 17:31:38 -0400

    fixed issues with randomTeams method

--- src/Driver.java
+++ src/Driver.java
@@ -4,11 +4,11 @@ public class Driver {
 	public static void main(String[] args) {
 		Schedule schedule = new Schedule();
 		System.out.println(schedule);
-		String[] _arr = new Array[10];
+		String[] _arr = new String[10];
 		String[] _players = players();
 	}
 	
-	public Array[] players(String[] _arr) {
+	public static String[] players(String[] _arr) {
 		Scanner myScanner = new Scanner(System.in);
 		System.out.println("Please enter the name of the 10 players");
 		for (i = 0; i <= 10;i++) {
